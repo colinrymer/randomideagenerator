6f490c991e69f87cb20a259e4385d77d1fd495d1
Colin Rymer <crymer11@gmail.com>
2012-07-11 15:08:52 -0400

    boostrapping it up

--- web.rb
+++ web.rb
@@ -36,8 +36,12 @@ class IdeaGenerator
   end
 end
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  redis = Redis.new
+end
 
 # Routes
 get '/' do

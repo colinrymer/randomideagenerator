bc503311c22fc989013fac62a9b4b7c8a2fee134
Colin Rymer <crymer11@gmail.com>
2012-07-18 11:20:39 -0400

    forgot cucumber test step

--- features/step_definitions/category_steps.rb
+++ features/step_definitions/category_steps.rb
@@ -1,26 +1,24 @@
 Given /^I am on the categories index page$/ do
   visit '/categories'
   page.status_code.should == 200
+  @new_category = 'this_is_an_obvious_test_category'
 end
 
 Then /^I should see a list of all the categories$/ do
-  pending # express the regexp above with the code you wish you had
+  page.should have_table('categories_table')
 end
 
 When /^I add a new category$/ do
-  @new_category = 'this_is_an_obvious_test_category'
   fill_in 'new_category', with: @new_category
   click_button 'Add Category'
 end
 
 Then /^I should see the new category added to the list$/ do
-  @new_category = 'this_is_an_obvious_test_category'
   current_path.should == '/categories'
   page.should have_content(@new_category)
 end
 
 When /^I remove a category$/ do
-  @new_category = 'this_is_an_obvious_test_category'
   unless page.should have_content(@new_category)
     fill_in 'new_category', with: @new_category
     click_button 'Add Category'
@@ -29,7 +27,6 @@ When /^I remove a category$/ do
 end
 
 Then /^I shouldn't see the removed category on the list$/ do
-  @new_category = 'this_is_an_obvious_test_category'
   current_path.should == '/categories'
   page.should_not have_content(@new_category)
 end
